113a4f1f9e3334d6fcf14a25e2acb8bb5a272152
Hiroshi Hayakawa <hhiroshell@gmail.com>
2019-10-04 22:14:00 +0900

    Upgrade terraform -> 0.12 | Use same code base between multi and single AD regions.

--- multi-ad/oke.tf
+++ multi-ad/oke.tf
@@ -1,60 +1,81 @@
-variable "oke_kubernetes_version" { default = "v1.12.7" }
-variable "oke_kubernetes_dashboard_enabled" { default = false }
-variable "oke_helm_tiller_enabled" { default = false }
+variable "oke_kubernetes_version" {
+  default = "v1.12.7"
+}
 
-variable "oke_kubernetes_node_version" { default = "v1.12.7" }
-variable "oke_node_pool_node_image_name" { default = "Oracle-Linux-7.6" }
-variable "oke_node_pool_shape" { default = [ "VM.Standard2.1" ] }
-variable "oke_node_pool_quantity_per_subnet" { default = [ 1 ] }
+variable "oke_kubernetes_dashboard_enabled" {
+  default = false
+}
+
+variable "oke_helm_tiller_enabled" {
+  default = false
+}
+
+variable "oke_kubernetes_node_version" {
+  default = "v1.12.7"
+}
+
+variable "oke_node_pool_node_image_name" {
+  default = "Oracle-Linux-7.6"
+}
+
+variable "oke_node_pool_shape" {
+  default = ["VM.Standard2.1"]
+}
+
+variable "oke_node_pool_quantity_per_subnet" {
+  default = [1]
+}
 
 locals {
-    oke_kube_config_expiration = 2592000
-    oke_kube_config_token_version = "1.0.0"
+  oke_kube_config_expiration    = 2592000
+  oke_kube_config_token_version = "1.0.0"
 }
 
 resource "oci_containerengine_cluster" "oke-cluster" {
-    compartment_id = "${var.compartment_ocid}"
-    name = "${var.oke_resource_prefix}-cluster"
-    kubernetes_version = "${var.oke_kubernetes_version}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    options {
-        service_lb_subnet_ids = [
-            "${oci_core_subnet.oke-sn-lb.id}",
-        ]
-        add_ons {
-            is_kubernetes_dashboard_enabled = "${var.oke_kubernetes_dashboard_enabled}"
-            is_tiller_enabled = "${var.oke_helm_tiller_enabled}"
-        }
+  compartment_id     = var.compartment_ocid
+  name               = "${var.oke_resource_prefix}-cluster"
+  kubernetes_version = var.oke_kubernetes_version
+  vcn_id             = oci_core_virtual_network.oke-vcn.id
+  options {
+    service_lb_subnet_ids = [
+      oci_core_subnet.oke-sn-lb.id,
+    ]
+    add_ons {
+      is_kubernetes_dashboard_enabled = var.oke_kubernetes_dashboard_enabled
+      is_tiller_enabled               = var.oke_helm_tiller_enabled
     }
+  }
 }
 
 resource "oci_containerengine_node_pool" "oke-node-pool" {
-    count = "${length(var.oke_node_pool_shape)}"
-    cluster_id = "${oci_containerengine_cluster.oke-cluster.id}"
-    compartment_id = "${var.compartment_ocid}"
-    name = "${var.oke_resource_prefix}-node-pool-${count.index}"
-    kubernetes_version = "${var.oke_kubernetes_node_version}"
-    node_image_name = "${var.oke_node_pool_node_image_name}"
-    node_shape = "${element(var.oke_node_pool_shape, count.index)}"
-    node_config_details {
-        # placement_configs = [
-        placement_configs {
-            # TODO: Upgrading to Terraform v0.12 and Dynamic configuration blocks.
-            # for_each = data.oci_identity_availability_domains.ads.availability_domains
-            availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains["0"], "name")}"
-            subnet_id = "${oci_core_subnet.oke-sn-w.id}"
-        }
-        size = "${element(var.oke_node_pool_quantity_per_subnet, 0)}"
+  count              = length(var.oke_node_pool_shape)
+  cluster_id         = oci_containerengine_cluster.oke-cluster.id
+  compartment_id     = var.compartment_ocid
+  name               = "${var.oke_resource_prefix}-node-pool-${count.index}"
+  kubernetes_version = var.oke_kubernetes_node_version
+  node_image_name    = var.oke_node_pool_node_image_name
+  node_shape         = element(var.oke_node_pool_shape, count.index)
+  node_config_details {
+    dynamic "placement_configs" {
+      for_each = [for ad in data.oci_identity_availability_domains.ads.availability_domains: {
+        name = ad.name
+      }]
+      content {
+        availability_domain = placement_configs.value.name
+        subnet_id           = oci_core_subnet.oke-sn-w.id
+      }
     }
+    size = element(var.oke_node_pool_quantity_per_subnet, count.index)
+  }
 }
 
 data "oci_containerengine_cluster_kube_config" "oke-kube-config" {
-    cluster_id = "${oci_containerengine_cluster.oke-cluster.id}"
-    expiration = "${local.oke_kube_config_expiration}"
-    token_version = "${local.oke_kube_config_token_version}"
+  cluster_id    = oci_containerengine_cluster.oke-cluster.id
+  expiration    = local.oke_kube_config_expiration
+  token_version = local.oke_kube_config_token_version
 }
 
 resource "local_file" "kubeconfig" {
-    content = "${data.oci_containerengine_cluster_kube_config.oke-kube-config.content}"
-    filename = "${path.module}/generated/kubeconfig"
+  content  = data.oci_containerengine_cluster_kube_config.oke-kube-config.content
+  filename = "${path.module}/generated/kubeconfig"
 }
--- multi-ad/vcn.tf
+++ multi-ad/vcn.tf
@@ -1,84 +1,80 @@
 locals {
-    cidr_public_internet = "0.0.0.0/0"
-    cidr_cluster_wide = "10.0.0.0/16"
-    cidr_vcn_loadbalancer = "10.0.20.0/24"
-    cidr_vcn_worker = "10.0.10.0/24"
+  cidr_public_internet  = "0.0.0.0/0"
+  cidr_cluster_wide     = "10.0.0.0/16"
+  cidr_vcn_loadbalancer = "10.0.20.0/24"
+  cidr_vcn_worker       = "10.0.10.0/24"
 }
 
 resource "oci_core_virtual_network" "oke-vcn" {
-    compartment_id = "${var.compartment_ocid}"
-    display_name = "${var.oke_resource_prefix}-oke-vcn"
-    cidr_block = "${local.cidr_cluster_wide}"
-    dns_label = "${var.oke_resource_prefix}"
+  compartment_id = var.compartment_ocid
+  display_name   = "${var.oke_resource_prefix}-oke-vcn"
+  cidr_block     = local.cidr_cluster_wide
+  dns_label      = var.oke_resource_prefix
 }
 
 resource "oci_core_internet_gateway" "oke-igw" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-igw"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-igw"
 }
 
 resource "oci_core_nat_gateway" "oke-ngw" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-ngw"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-ngw"
 }
 
 resource "oci_core_route_table" "oke-rt-igw" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-rt-igw"
-    route_rules {
-        destination = "${local.cidr_public_internet}"
-        network_entity_id = "${oci_core_internet_gateway.oke-igw.id}"
-    }
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-rt-igw"
+  route_rules {
+    destination       = local.cidr_public_internet
+    network_entity_id = oci_core_internet_gateway.oke-igw.id
+  }
 }
 
 resource "oci_core_route_table" "oke-rt-ngw" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-rt-ngw"
-    route_rules {
-        destination = "${local.cidr_public_internet}"
-        network_entity_id = "${oci_core_nat_gateway.oke-ngw.id}"
-    }
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-rt-ngw"
+  route_rules {
+    destination       = local.cidr_public_internet
+    network_entity_id = oci_core_nat_gateway.oke-ngw.id
+  }
 }
 
 resource "oci_core_security_list" "oke-sl-lb-public-access" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-sl-lb-public-access"
-    egress_security_rules = [
-        {
-            stateless = true
-            destination = "${local.cidr_public_internet}"
-            protocol = "6"
-        }
-    ]
-    ingress_security_rules = [
-        # {
-        #     stateless = true
-        #     source = "${local.cidr_public_internet}"
-        #     protocol = "6"
-        #     tcp_options {
-        #         max = "80"
-        #         min = "80"
-        #     }
-        # },{
-        #     stateless = true
-        #     source = "${local.cidr_public_internet}"
-        #     protocol = "6"
-        #     tcp_options {
-        #         max = "443"
-        #         min = "443"
-        #     }
-        # }
-        {
-            stateless = true
-            source = "${local.cidr_public_internet}"
-            protocol = "6"
-        }
-    ]
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-sl-lb-public-access"
+  egress_security_rules {
+    stateless   = true
+    destination = local.cidr_public_internet
+    protocol    = "6"
+  }
+  ingress_security_rules {
+    # {
+    #     stateless = true
+    #     source = "${local.cidr_public_internet}"
+    #     protocol = "6"
+    #     tcp_options {
+    #         max = "80"
+    #         min = "80"
+    #     }
+    # },{
+    #     stateless = true
+    #     source = "${local.cidr_public_internet}"
+    #     protocol = "6"
+    #     tcp_options {
+    #         max = "443"
+    #         min = "443"
+    #     }
+    # }
+    stateless = true
+    source    = local.cidr_public_internet
+    protocol  = "6"
+  }
 }
 
 # resource "oci_core_security_list" "oke-sl-w-between-workers" {
@@ -118,16 +114,14 @@ resource "oci_core_security_list" "oke-sl-lb-public-access" {
 # }
 
 resource "oci_core_security_list" "oke-sl-w-to-external-services" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-sl-w-to-external-services"
-    egress_security_rules = [
-        {
-            stateless = false
-            destination = "${local.cidr_public_internet}"
-            protocol = "all"
-        }
-    ]
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-sl-w-to-external-services"
+  egress_security_rules {
+    stateless   = false
+    destination = local.cidr_public_internet
+    protocol    = "all"
+  }
 }
 
 # resource "oci_core_security_list" "oke-sl-w-healthcheck-from-master" {
@@ -188,88 +182,53 @@ resource "oci_core_security_list" "oke-sl-w-to-external-services" {
 # }
 
 resource "oci_core_security_list" "oke-sl-w-optional" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-sl-w-optional"
-    ingress_security_rules = [
-        {
-            stateless = false
-            source = "${local.cidr_cluster_wide}"
-            protocol = "6"
-            tcp_options {
-                max = "22"
-                min = "22"
-            }
-        },{
-            stateless = false
-            source = "${local.cidr_cluster_wide}"
-            protocol = "6"
-            tcp_options {
-                max = "32767"
-                min = "30000"
-            }
-        }
-    ]
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-sl-w-optional"
+  ingress_security_rules {
+    stateless = false
+    source    = local.cidr_cluster_wide
+    protocol  = "6"
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+  ingress_security_rules {
+    stateless = false
+    source    = local.cidr_cluster_wide
+    protocol  = "6"
+    tcp_options {
+      max = "32767"
+      min = "30000"
+    }
+  }
 }
 
-# resource "oci_core_subnet" "oke-sn-lb" {
-#     count = "${length(data.oci_identity_availability_domains.ads.availability_domains) > 2 ? 2 : 1}"
-#     compartment_id = "${var.compartment_ocid}"
-#     vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-#     display_name = "${var.oke_resource_prefix}-oke-sn-lb-ad${count.index}"
-#     availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains["${count.index}"], "name")}"
-#     cidr_block = "${local.cidr_vcn_loadbalancer["${count.index}"]}"
-#     prohibit_public_ip_on_vnic = false
-#     route_table_id = "${oci_core_route_table.oke-rt-igw.id}"
-#     security_list_ids = [
-#         "${oci_core_security_list.oke-sl-lb-public-access.id}",
-#     ]
-#     dns_label = "lb${count.index}"
-# }
-
-# resource "oci_core_subnet" "oke-sn-w" {
-#     count = "${length(data.oci_identity_availability_domains.ads.availability_domains)}"
-#     compartment_id = "${var.compartment_ocid}"
-#     vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-#     display_name = "${var.oke_resource_prefix}-oke-sn-w-ad${count.index}"
-#     availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains["${count.index}"], "name")}"
-#     cidr_block = "${local.cidr_vcn_worker["${count.index}"]}"
-#     prohibit_public_ip_on_vnic = true
-#     route_table_id = "${oci_core_route_table.oke-rt-ngw.id}"
-#     security_list_ids = [
-#         "${oci_core_security_list.oke-sl-w-between-workers.id}",
-#         "${oci_core_security_list.oke-sl-w-to-external-services.id}",
-#         # "${oci_core_security_list.oke-sl-w-optional.id}",
-#     ]
-#     dns_label = "w${count.index}"
-# }
-
 resource "oci_core_subnet" "oke-sn-lb" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-sn-lb"
-    # availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains[0], "name")}"
-    cidr_block = "${local.cidr_vcn_loadbalancer}"
-    prohibit_public_ip_on_vnic = false
-    route_table_id = "${oci_core_route_table.oke-rt-igw.id}"
-    security_list_ids = [
-        "${oci_core_security_list.oke-sl-lb-public-access.id}"
-    ]
-    dns_label = "lb"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-sn-lb"
+  cidr_block                 = local.cidr_vcn_loadbalancer
+  prohibit_public_ip_on_vnic = false
+  route_table_id             = oci_core_route_table.oke-rt-igw.id
+  security_list_ids = [
+    oci_core_security_list.oke-sl-lb-public-access.id,
+  ]
+  dns_label = "lb"
 }
 
 resource "oci_core_subnet" "oke-sn-w" {
-    compartment_id = "${var.compartment_ocid}"
-    vcn_id = "${oci_core_virtual_network.oke-vcn.id}"
-    display_name = "${var.oke_resource_prefix}-oke-sn-w"
-    # availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains[0], "name")}"
-    cidr_block = "${local.cidr_vcn_worker}"
-    prohibit_public_ip_on_vnic = true
-    route_table_id = "${oci_core_route_table.oke-rt-ngw.id}"
-    security_list_ids = [
-        # "${oci_core_security_list.oke-sl-w-between-workers.id}",
-        "${oci_core_security_list.oke-sl-w-to-external-services.id}",
-        # "${oci_core_security_list.oke-sl-w-optional.id}",
-    ]
-    dns_label = "w"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.oke-vcn.id
+  display_name   = "${var.oke_resource_prefix}-oke-sn-w"
+  cidr_block                 = local.cidr_vcn_worker
+  prohibit_public_ip_on_vnic = true
+  route_table_id             = oci_core_route_table.oke-rt-ngw.id
+  security_list_ids = [
+    oci_core_security_list.oke-sl-w-to-external-services.id,
+    # oci_core_security_list.oke-sl-w-optional.id,
+  ]
+  dns_label = "w"
 }
+
